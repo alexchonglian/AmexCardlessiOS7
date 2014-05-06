8b0e2202aa180aca06337f301d433ee867ec10bd
Chong Lian <alexchonglian@gmail.com>
2014-05-06 02:21:12 -0400

    created customized UI component to add/remove item from menu

--- CardlessPayment/MenuTVC.m
+++ CardlessPayment/MenuTVC.m
@@ -63,48 +63,59 @@
     //UIStepper* stepper = [[UIStepper alloc] init];
     //stepper.frame = CGRectMake(210, 20, 20, 20);
 
+    UIButton *expandButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    expandButton.layer.cornerRadius = 15;
+    expandButton.layer.masksToBounds = YES;
+    expandButton.frame = CGRectMake(280, 20, 30, 30);
+    [expandButton setTitle:@"+" forState:UIControlStateNormal];
+    [expandButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [expandButton setBackgroundColor:[UIColor colorWithRed:(CGFloat)0.8f green:(CGFloat)0.8f blue:(CGFloat)0.8f alpha:(CGFloat)1.0f]];
+    [expandButton addTarget:self action:@selector(expandManipulator:) forControlEvents:UIControlEventTouchUpInside];
+    expandButton.tag = indexPath.row;
     
-    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    addButton.layer.cornerRadius = 5;
-    addButton.layer.masksToBounds = YES;
-    addButton.frame = CGRectMake(100, 5, 20, 20);
-
-    [addButton setTitle:@"+" forState:UIControlStateNormal];
-    [addButton setBackgroundColor:[UIColor whiteColor]];
-    [addButton addTarget:self action:@selector(printOut:) forControlEvents:UIControlEventTouchUpInside];
-    addButton.tag = indexPath.row;
     
     
+    UILabel *qtyLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, 6, 27, 33)];
+    qtyLabel.layer.cornerRadius = 5;
+    qtyLabel.layer.masksToBounds = YES;
+    //[qtyLabel setBackgroundColor:[UIColor whiteColor]];
+    qtyLabel.text = @"0";
     
     UIButton *delButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     delButton.layer.cornerRadius = 5;
     delButton.layer.masksToBounds = YES;
-    delButton.frame = CGRectMake(50, 5, 20, 20);
+    delButton.frame = CGRectMake(6, 6, 33, 33);
     [delButton setTitle:@"-" forState:UIControlStateNormal];
     [delButton setBackgroundColor:[UIColor whiteColor]];
-    [delButton addTarget:self action:@selector(printOut:) forControlEvents:UIControlEventTouchUpInside];
+    [delButton addTarget:self action:@selector(decrementQty:) forControlEvents:UIControlEventTouchUpInside];
     delButton.tag = indexPath.row;
     
     
-    UILabel *qty = [[UILabel alloc] initWithFrame:CGRectMake(75, 5, 20, 20)];
-    qty.layer.cornerRadius = 5;
-    qty.layer.masksToBounds = YES;
-    //[qty setBackgroundColor:[UIColor whiteColor]];
-    qty.text = @" 0";
+    
+    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    addButton.layer.cornerRadius = 5;
+    addButton.layer.masksToBounds = YES;
+    addButton.frame = CGRectMake(70, 6, 33, 33);
+    [addButton setTitle:@"+" forState:UIControlStateNormal];
+    [addButton setBackgroundColor:[UIColor whiteColor]];
+    [addButton addTarget:self action:@selector(incrementQty:) forControlEvents:UIControlEventTouchUpInside];
+    addButton.tag = indexPath.row;
+    
     
     UIButton *collapseButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     //addButton.frame = CGRectMake(X_ADDBUTTON, Y_BUTTON, WIDTH_BUTTON, HEIGHT_BUTTON);
-    collapseButton.frame = CGRectMake(5, 5, 40, 20);
-    
-    [collapseButton setTitle:@">" forState:UIControlStateNormal];
-    [collapseButton setBackgroundColor:[UIColor whiteColor]];
-    [collapseButton addTarget:self action:@selector(printOut:) forControlEvents:UIControlEventTouchUpInside];
+    collapseButton.frame = CGRectMake(109, 6, 45, 33);
+    //[collapseButton setBackgroundColor:[UIColor whiteColor]];
+    [collapseButton setTitle:@"    √" forState:UIControlStateNormal];
+    [collapseButton addTarget:self action:@selector(collapseManipulatorAndAddItemToCart:) forControlEvents:UIControlEventTouchUpInside];
     collapseButton.tag = indexPath.row;
     
-    UIView *manipulator = [[UIView alloc] initWithFrame:CGRectMake(180, 34, 128, 30)];
+    
+    
+    UIView *manipulator = [[UIView alloc] initWithFrame:CGRectMake(153, 10, 160, 45)];
     //[manipulator setUserInteractionEnabled:YES];
     manipulator.opaque = YES;
-    [manipulator setBackgroundColor:[UIColor grayColor]];
+    [manipulator setBackgroundColor:[UIColor colorWithRed:(CGFloat)0.8f green:(CGFloat)0.8f blue:(CGFloat)0.8f alpha:(CGFloat)1.0f]];
     //[manipulator addTarget:self action:@selector(printOut:) forControlEvents:UIControlEventTouchUpInside];
     manipulator.tag = indexPath.row;
     manipulator.layer.cornerRadius = 8;
@@ -122,23 +133,108 @@
     [cell.contentView addSubview:label];
      */
     
+
     
-    //[cell.contentView addSubview:addButton];
-    //[cell.contentView addSubview:delButton];
-    //[cell.contentView addSubview: stepper];
+    
+    [cell.contentView addSubview:expandButton];
+    
     [cell.contentView addSubview:manipulator];
     
     [manipulator addSubview:addButton];
     [manipulator addSubview:delButton];
-    [manipulator addSubview:qty];
+    [manipulator addSubview:qtyLabel];
     [manipulator addSubview:collapseButton];
-    //manipulator.hidden = YES;
+    manipulator.hidden = YES;
     
     return cell;
 }
 
+- (IBAction)expandManipulator: (UIButton *)sender {
+    UIButton *expand = (UIButton *)sender;
+    UIView *cellContentView = expand.superview;
+    for (UIView *manipulator in cellContentView.subviews) {
+        //NSLog(@"%@", view);
+        if([manipulator isMemberOfClass:[UIView class]]) {
+            
+            for (UIView *manipulatorComponent in manipulator.subviews) {
+                if([manipulatorComponent isMemberOfClass:[UILabel class]]) {
+                    UILabel *label = (UILabel *)manipulatorComponent;
+                    int qty = [label.text intValue];
+                    if (qty == 0) {
+                        label.text = @"1";
+                    }
+                }
+            }
+            manipulator.hidden = NO;
+        }
+    }
+    expand.hidden = YES;
+}
 
-- (void)printOut: (UIButton *)sender {
+
+
+- (IBAction)collapseManipulatorAndAddItemToCart: (UIButton *)sender {
+    UIButton *collapse = (UIButton *)sender;
+    
+    int qty = 0;
+    
+    for (UIView *manipulatorComponent in collapse.superview.subviews) {
+        if ([manipulatorComponent isMemberOfClass:[UILabel class]]) {
+            UILabel *label = (UILabel *)manipulatorComponent;
+            qty = [label.text intValue];
+        }
+    }
+    
+    UIView *cellContentView = collapse.superview.superview;
+    for (UIView *cellComponent in cellContentView.subviews) {
+        //NSLog(@"%@", view);
+        if([cellComponent isMemberOfClass:[UIButton class]]) {
+            UIButton *expandBtn = (UIButton *) cellComponent;
+            if (qty > 0) {
+                [expandBtn setTitle:[NSString stringWithFormat:@"%d", qty] forState:UIControlStateNormal];
+                [expandBtn setBackgroundColor:[UIColor redColor]];
+            } else {
+                [expandBtn setTitle:@"+" forState:UIControlStateNormal];
+                [expandBtn setBackgroundColor:[UIColor colorWithRed:(CGFloat)0.8f green:(CGFloat)0.8f blue:(CGFloat)0.8f alpha:(CGFloat)1.0f]];
+            }
+            expandBtn.hidden = NO;
+        }
+    }
+    collapse.superview.hidden = YES;
+}
+
+
+- (IBAction)decrementQty:(UIButton *)sender {
+    [self changeQty: sender by:-1];
+}
+
+- (IBAction)incrementQty: (UIButton *)sender {
+    [self changeQty: sender by:1];
+}
+
+
+- (IBAction)changeQty: (UIButton *)sender by:(int)num{
+    UIButton *addOrSubButton = (UIButton *)sender;
+    UIView *cellContentView = addOrSubButton.superview.superview;
+    for (UIView *view in cellContentView.subviews) {
+        //NSLog(@"%@", view);
+        if([view isMemberOfClass:[UIView class]]) {
+            for (UIView *subview in view.subviews) {
+                if([subview isMemberOfClass:[UILabel class]]) {
+                    UILabel *label = (UILabel*)subview;
+                    int qty = [label.text intValue];
+                    if(qty > 0 || num == 1) {
+                        qty += num;
+                    }
+                    label.text = [NSString stringWithFormat:@"%d", qty];
+                }
+            }
+
+        }
+    }
+}
+
+- (IBAction)printOut: (UIButton *)sender {
     UIButton *button = (UIButton *)sender;
     NSLog(@"%@ %@", [self titleForRow:button.tag],[self subtitleForRow:button.tag]);
     NSString *newEntry = [NSString stringWithFormat:@"%@ %@", [self titleForRow:button.tag],[self subtitleForRow:button.tag]];
